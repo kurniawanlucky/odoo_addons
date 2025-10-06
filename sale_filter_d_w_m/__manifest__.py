e5a558cd41ea295018cd6a014a650f283dec8b0f
lucky <kurniawanluckyy@gmail.com>
2025-10-07 06:23:50 +0800

    [ADD][18.0]sale_filter_d_w_m

--- sale_filter_d_w_m/__manifest__.py
+++ sale_filter_d_w_m/__manifest__.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-# © 2017
+# © 2025
 #   @Lucky Kurniawan <kurniawanluckyy@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Sale Filter Today, Week, Month, Year",
     "summary": "Add Filter Today, This Week, This Month,and This year",
-    "version": "10.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Lucky Kurniawan ",
-    "website": "https://github.com/kurniawanlucky/Odoo10.0",
+    "website": "https://github.com/kurniawanlucky/odoo_addons",
     "category": "Sale",
     "depends": ['sale'],
     "data": [
         'views/sale_views.xml',
     ],
-    "images": ['static/description/icon.png'],
     "license": "AGPL-3",
     "installable": True,
     "application": True,
