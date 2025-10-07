fea5ee0de5882fda6f3f5f0967eb38529cf4e73c
lucky <kurniawanluckyy@gmail.com>
2025-10-08 06:56:23 +0800

    test

--- purchase_filter_d_w_m/__manifest__.py
+++ purchase_filter_d_w_m/__manifest__.py
@@ -3,17 +3,26 @@
 #   @Lucky Kurniawan <kurniawanluckyy@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Purchase Filter Today, Week, Month, Year",
-    "summary": "Add Filter Today, This Week, This Month,and This year",
-    "version": "18.0.1.0.0",
-    "author": "Lucky Kurniawan ",
-    "website": "https://github.com/kurniawanlucky/odoo_addons",
-    "category": "Purchase",
-    "depends": ['purchase'],
-    "data": [
+    'name': 'Purchase Filter Today, Week, Month, Year',
+    'summary': 'Filter Purchase Orders by Today, This Week, This Month, and This Year',
+    'description': '''
+    Filter Purchase Orders quickly by date ranges.
+    ----------------------------------------------------
+    - Today
+    - This Week
+    - This Month
+    - This Year
+        ''',
+    'version': '18.0.1.0.0',
+    'author': 'Lucky Kurniawan ',
+    'website': 'https://github.com/kurniawanlucky/odoo_addons',
+    'category': 'Purchase',
+    'depends': ['purchase'],
+    'data': [
         'views/purchase_views.xml',
     ],
-    "license": "AGPL-3",
-    "installable": True,
-    "application": True,
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
 }
