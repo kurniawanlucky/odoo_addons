85012e651124dfb52a8e0954171fcd8c8266f33f
lucky <kurniawanluckyy@gmail.com>
2025-10-08 06:47:58 +0800

    [ADD][18.0]purchase_filter_d_w_m

---
+++ purchase_filter_d_w_m/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017
+#   @Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Purchase Filter Today, Week, Month, Year",
+    "summary": "Add Filter Today, This Week, This Month,and This year",
+    "version": "18.0.1.0.0",
+    "author": "Lucky Kurniawan ",
+    "website": "https://github.com/kurniawanlucky/odoo_addons",
+    "category": "Purchase",
+    "depends": ['purchase'],
+    "data": [
+        'views/purchase_views.xml',
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+}
