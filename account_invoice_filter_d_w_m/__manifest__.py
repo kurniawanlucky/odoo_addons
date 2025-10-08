54b632173c32f819b6fb84900f55b80107c84f20
lucky <kurniawanluckyy@gmail.com>
2025-10-09 06:40:38 +0800

    [ADD][18.0]account_invoice_filter_d_w_m

---
+++ account_invoice_filter_d_w_m/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2025
+#   @Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Invoice Filter by Date',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Filter Invoices by Today, This Week, This Month, and This Year',
+    'description': """
+Easily filter and analyze your customer invoices by specific date ranges.
+----------------------------------------------------
+- Today
+- This Week
+- This Month
+- This Year
+    """,
+    'author': 'Lucky Kurniawan',
+    'depends': ['account'],
+    'images': ['static/description/banner.png'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
--- purchase_filter_d_w_m/__manifest__.py
+++ purchase_filter_d_w_m/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2017
+# © 2025
 #   @Lucky Kurniawan <kurniawanluckyy@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
