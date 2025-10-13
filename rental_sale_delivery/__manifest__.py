512b655abc9588fa68feed6afd434676d989243c
lucky <kurniawanluckyy@gmail.com>
2025-10-14 05:48:31 +0800

    [ADD][18.0]rental_sale_delivery

---
+++ rental_sale_delivery/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2025 Lucky Kurniawan <kurniawanluckyy@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Sale Rental Delivery',
+    'summary': '',
+    'version': '18.0.1.0.0',
+    'author': 'Lucky Kurniawan',
+    'website': 'https://github.com/kurniawanlucky/odoo_addons',
+    'category': 'Sales',
+    'depends': ['rental_sale_product', 'delivery'],
+    'data': [
+        'data/rental_delivery_data.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
+}
